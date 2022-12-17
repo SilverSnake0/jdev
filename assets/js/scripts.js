12ea2ff08c3dba504834c62303e3c914180087ce
SilverSnake0 <105333801+SilverSnake0@users.noreply.github.com>
2022-12-17 15:24:56 -0500

    Organized code

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -16,53 +16,6 @@
     });
 });
 
-// Define the data for the circle graph
-const data = {
-    labels: ["Python", "JavaScript", "HTML", "CSS", "SQL"], // rename the labels to match the coding languages
-    datasets: [{
-        data: [75, 40, 35, 35, 35], // set the data for each label
-        backgroundColor: [
-            "#F7464A",
-            "#FDB45C",
-            "#46BFBD",
-            "#949FB1",
-            "#4D5360"
-        ],
-        hoverBackgroundColor: [
-            "#FF5A5E",
-            "#FFC870",
-            "#5AD3D1",
-            "#A8B3C5",
-            "#616774"
-        ]
-    }]
-};
-
-// Define the options for the circle graph
-const options = {
-    animation: {
-        animateScale: true, // enable animation
-        animateRotate: true
-    }
-};
-
-// Create the circle graph
-const char = document.getElementById("myChart").getContext("2d");
-const myChart = new Chart(char, {
-    type: "doughnut",
-    data: data,
-    options: options
-});
-
-// Show the circle graph
-function showGraph() {
-    document.getElementById('myChart').style.display = 'block';
-}
-
-function hideGraph() {
-    document.getElementById('myChart').style.display = 'none';
-}
-
 // Create a canvas element and set its width and height
 var canvas = document.getElementById("particle-bg");
 canvas.width = window.innerWidth;
